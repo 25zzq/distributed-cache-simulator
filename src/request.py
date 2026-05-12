63faa1a400b172b2ca4a8c9874add674a5ea5907
25zzq <jaydenbuckley.ct@gmail.com>
2026-05-12 19:22:55 -0400

    added comments detailing functionality of request object's methods

--- src/request.py
+++ src/request.py
@@ -13,14 +13,18 @@ class Request():
         self.processing_time_ms = processing_time_ms
         self.retry_count = retry_count
     
+    # can_retry and retry will recursively call each other until max retries are met
     def can_retry(self):
+        #checks if a request has exceeded it's retry count and creates a boolean that will be used for the retry function
         if self.retry_count >= self.max_retries:
             return False
         else:
             return True
     
     def retry(self):
+        #uses the returned boolean value from can_retry to determine what happens next with a request in retrial stage
         if self.can_retry():
+            #checks boolean value and increments the retry count until the max retries is met
             self.retry_count += 1
             self.status = "Retrying"
         else:
